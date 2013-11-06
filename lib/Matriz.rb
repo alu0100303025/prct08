3bf3493c287af7dd59bb448f94659160f171c929
Abraham <abrahamblg@hotmail.es>
2013-11-06 10:52:36 +0000

    Trabajo en la clase

--- lib/Matriz.rb
+++ lib/Matriz.rb
@@ -1,25 +1,42 @@
 class Matriz
    attr_reader :filas,:columnas, :valor, :matriz, :m1, :m2
    attr_writer :resultado
-   include Comparable
 
-def muestra_matriz(matriz)
-	i=0
-	rango = 0...matriz[0].length
-	rango_txt = rango.to_a
-	print "\n   #{rango_txt.join("  ")}\n"
-	for fila in matriz
-		puts "#{i} #{fila}"
-		i += 1
-	end
+def initialize(fil, col)
+	@filas = fil;
+	@columnas = col;
 end
+
+
+def to_s
+        i=0
+	txt = "";
+        rango = 0...matriz[0].length
+        rango_txt = rango.to_a
+        txt += "\n   #{rango_txt.join("  ")}\n"
+        for fila in matriz
+                txt += "#{i} #{fila}\n"
+                i += 1
+        end
+	return txt
+end
+#def muestra_matriz(matriz)
+#	i=0
+#	rango = 0...matriz[0].length
+#	rango_txt = rango.to_a
+#	print "\n   #{rango_txt.join("  ")}\n"
+#	for fila in matriz
+#		puts "#{i} #{fila}"
+#		i += 1
+#	end
+#end
 
 def hacer_matriz(filas,columnas, valor)
 	array_devolucion = Array.new(filas){Array.new(columnas, valor)}
 end
 
-def multiplica_matrices(m1,m2)
-        dimensiones=[[m1.length, m1[0].length],[m2.length, m2[0].length]]
+def *(m2)
+        dimensiones=[[@filas, @columnas,m2.filas, m2.columnas]]
         filas_final = dimensiones[0][0]
         columnas_final = dimensiones[1][1]
         resultado = Array.new(filas_final){Array.new(columnas_final, 0)}
@@ -27,7 +44,7 @@ def multiplica_matrices(m1,m2)
                 for j in 0...dimensiones[1][1]
                         temp = Array.new(dimensiones[0][0],0)
                         for k in 0...dimensiones[0][1]
-                                val1 = m1[i][k]
+                                val1 = matriz[i][k]
                                 val2 = m2[k][j]
                                 temp[k] += val1 * val2
                         end
@@ -38,16 +55,38 @@ def multiplica_matrices(m1,m2)
 end
 
 
-def suma_matrices(m1,m2)
-	dimensiones=[[m1.length, m1[0].length],[m2.length, m2[0].length]]
+def +(m2)
+        dimensiones=[[@filas, @columnas,m2.filas, m2.columnas]]
 	filas_final = dimensiones[0][0]
 	columnas_final = dimensiones[1][1]
 	resultado = Array.new(filas_final){Array.new(columnas_final, 0)}
 	for i in 0...dimensiones[0][0]
 		for j in 0...dimensiones[1][1]
-			resultado[i][j] = m1[i][j] + m2[i][j]
+			resultado[i][j] = @matriz[i][j] + m2[i][j]
 		end 
 	end
 	return resultado
 end
+
+def []=(i,j,x)
+   @matriz[i][j] = x
+end
+
+def [](i,j)
+  @matriz[i][j]
+end
+
+def =(m1)
+   filas = m1.filas
+   columnas = m1.columnas
+   matriz =  Array.new(filas){Array.new(columnas)}
+   for i in 0...filas
+       for j in 0...columnas
+          matriz[i][j] = m1[i][j]
+       end
+   end
+   resultado = Matriz.new(filas, columnas)
+   
+end
+
 end
